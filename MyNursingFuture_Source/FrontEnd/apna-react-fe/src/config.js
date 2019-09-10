692e34240e7d06c629401979fa9f61053a22b56c
sanyam <sanyam@pageuppeople.com>
2019-09-10 10:00:39 +1000

    Settings

--- MyNursingFuture_Source/FrontEnd/apna-react-fe/src/config.js
+++ MyNursingFuture_Source/FrontEnd/apna-react-fe/src/config.js
@@ -1,15 +1,14 @@
 var conf;
 
-
 conf = {
-    apiUrl: 'http://localhost:50565/',
-    apiBaseUrl: 'api/',
-    siteUrl: 'http://localhost:50565/',
-    shareBaseUrl: 'http://localhost:50565/',
-    imagesDirectory: 'Content/img/'
-  };
-  
-  
+  apiUrl: 'http://localhost:50565/',
+  apiBaseUrl: 'api/',
+  siteUrl: 'http://localhost:50565/',
+  shareBaseUrl: 'http://localhost:50565/',
+  imagesDirectory: 'Content/img/'
+};
+
+
 // if (process.env.NODE_ENV == 'production'){
   // conf = {
     // apiUrl: 'http://180.235.131.104/',
@@ -19,20 +18,28 @@ conf = {
     // imagesDirectory: 'app/Content/img/'
   // };
 // } else {
-  // conf = {
-      // apiUrl: 'http://13.73.105.110',
-    // apiBaseUrl: '/api/',
-    // siteUrl: 'http://13.73.105.110',
-    // shareBaseUrl: 'http://13.73.105.110',
-    // imagesDirectory: '/Content/img/'
-  // };
-  // conf = {
-    // apiUrl: 'http://180.235.131.104/',
-    // apiBaseUrl: 'app/api/',
-    // siteUrl: 'http://180.235.131.104/',
-    // shareBaseUrl: 'http://180.235.131.104/',
-    // imagesDirectory: 'app/Content/img/'
-  // };
-// }
+//   conf = {
+//     apiUrl: 'http://180.235.131.104/',
+//     apiBaseUrl: 'app/api/',
+//     siteUrl: 'http://180.235.131.104/',
+//     shareBaseUrl: 'http://180.235.131.104/',
+//     imagesDirectory: 'app/Content/img/'
+//   };
+// } else {
+//   // conf = {
+//   //     apiUrl: 'http://13.73.105.110',
+//   //   apiBaseUrl: '/api/',
+//   //   siteUrl: 'http://13.73.105.110',
+//   //   shareBaseUrl: 'http://13.73.105.110',
+//   //   imagesDirectory: '/Content/img/'
+//   // };
+//   conf = {
+//     apiUrl: 'http://180.235.131.104/',
+//     apiBaseUrl: 'app/api/',
+//     siteUrl: 'http://180.235.131.104/',
+//     shareBaseUrl: 'http://180.235.131.104/',
+//     imagesDirectory: 'app/Content/img/'
+//   };
+
 
 export default conf;
