f4677932d14e703c2cc42955a3e8419bf9e71d75
Henry Pham <ntpha13@student.monash.edu>
2019-10-20 05:12:17 +1100

    update connetion strings

--- MyNursingFuture_Source/FrontEnd/apna-react-fe/src/config.js
+++ MyNursingFuture_Source/FrontEnd/apna-react-fe/src/config.js
@@ -1,7 +1,7 @@
 var conf;
 
 conf = {
-    apiUrl: 'http://localhost:5056/',
+    apiUrl: 'https://mynursingfutureapi.azurewebsites.net/',
     apiBaseUrl: 'api/',
     siteUrl: 'http://localhost:3000/',
     shareBaseUrl: 'http://localhost:3000/',
