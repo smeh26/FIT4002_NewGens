4cb69c37156f209ad20e89a7a1a9c9636699c8a5
Zayan Ikram <zayan.ikramw@gmail.com>
2019-11-08 00:58:21 +1100

    fix config

--- MyNursingFuture_Source/FrontEnd/apna-react-fe/src/config.js
+++ MyNursingFuture_Source/FrontEnd/apna-react-fe/src/config.js
@@ -1,53 +1,11 @@
 var conf;
 
 conf = {
-<<<<<<< HEAD
-  apiUrl: 'https://mynursingfutureapi.azurewebsites.net/',
-  // apiUrl: 'http://localhost:5056/',
-  apiBaseUrl: 'api/',
-  siteUrl: 'http://localhost:3000/',
-  shareBaseUrl: 'http://localhost:3000/',
-  imagesDirectory: 'img/'
-};
-
-
-// if (process.env.NODE_ENV == 'production'){
-    // apiUrl: 'http://180.235.131.104/',
-    // apiBaseUrl: 'app/api/',
-    // siteUrl: 'http://180.235.131.104/',
-    // shareBaseUrl: 'http://180.235.131.104/',
-    // imagesDirectory: 'app/Content/img/'
-// } else {
-//   conf = {
-//     apiUrl: 'http://180.235.131.104/',
-//     apiBaseUrl: 'app/api/',
-//     siteUrl: 'http://180.235.131.104/',
-//     shareBaseUrl: 'http://180.235.131.104/',
-//     imagesDirectory: 'app/Content/img/'
-//   };
-// } else {
-//   // conf = {
-//   //     apiUrl: 'http://13.73.105.110',
-//   //   apiBaseUrl: '/api/',
-//   //   siteUrl: 'http://13.73.105.110',
-//   //   shareBaseUrl: 'http://13.73.105.110',
-//   //   imagesDirectory: '/Content/img/'
-//   // };
-//   conf = {
-//     apiUrl: 'http://180.235.131.104/',
-//     apiBaseUrl: 'app/api/',
-//     siteUrl: 'http://180.235.131.104/',
-//     shareBaseUrl: 'http://180.235.131.104/',
-//     imagesDirectory: 'app/Content/img/'
-//   };
-
-=======
     apiUrl: 'https://mynursingfutureapi.azurewebsites.net/',
     apiBaseUrl: 'api/',
     siteUrl: 'http://localhost:3000/',
     shareBaseUrl: 'http://localhost:3000/',
     imagesDirectory: 'img/'
   };
->>>>>>> bc980297bb8da53481415626ba19c77f0623152c
 
 export default conf;
